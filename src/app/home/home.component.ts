423c0119ba0708d67a90a4ea6ebe5f7be85a2201
dharmendra_kumar <dharmendra_kumar>
2020-06-13 10:24:56 +0530

    meta

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = 'Demo';
-  constructor() { }
+  constructor( private meta: Meta ) { }
 
   ngOnInit(): void {
+   
+    this.meta.updateTag({ name: 'viewport', content: 'width=device-width, initial-scale=1' });
+  
+    this.meta.updateTag({ name: 'viewport', content: 'minimum-scale=1.0, maximum-scale=1.0,width=device-width, user-scalable=no' });
+    
+     $(document).ready(function() {
+         $("head").append('<meta http-equiv="refresh" content="1">');
+         this.addTag()
+     });
+
+
   }
+
+  addTag(){
+  var link = document.createElement('meta');
+  link.setAttribute('name', 'apple-mobile-web-app-status-bar-style');
+  link.content = "black-translucent";
+  document.getElementsByTagName('head')[0].appendChild(link);
+  }
+
 
 }
